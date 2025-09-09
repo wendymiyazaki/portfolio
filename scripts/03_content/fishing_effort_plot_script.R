630a4929ffe98bc8a094d8e064a7efc4d1a5ff85
kpolatchek7 <kpolatchek@gmail.com>
2025-09-09 09:25:48 -0400

    Code for change point to hot pink

--- scripts/03_content/fishing_effort_plot_script.R
+++ scripts/03_content/fishing_effort_plot_script.R
@@ -8,7 +8,7 @@ data("data_fishing_effort")
 # Create a simple plot
 p <- ggplot(data_fishing_effort,
        aes(x = month, y = effort_hours)) +
-  geom_point()
+  geom_point(color= "hotpink")
 
 # Save plot
 ggsave(plot = p, filename = "results/img/first_plot.png")
